82b372a0b780fc4a8b2051da45782029cfc915fc
Adelemphii <demilitioowner@gmail.com>
2025-02-02 18:23:45 -0600

    tests

---
+++ utils/list_helper.js
@@ -0,0 +1,42 @@
+const _ = require('lodash')
+
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  let likes = 0
+  blogs.forEach(element => {
+    likes += element.likes
+  })
+  return likes
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, blog) => blog.likes > max.likes ? blog : max, blogs[0])
+}
+
+const mostBlogs = (blogs) => {
+  const authorCounts = _.countBy(blogs, 'author')
+  const mostBlogs = _.maxBy(_.toPairs(authorCounts), pair => pair[1])
+  return {
+    author: mostBlogs[0],
+    blogs: mostBlogs[1]
+  }
+}
+
+const mostLikes = (blogs) => {
+  const authorBlogs = _.groupBy(blogs, 'author')
+  const sumLikes = _.mapValues(authorBlogs, (blogs) => _.sumBy(blogs, 'likes'))
+  const maxLikesAuthor = _.maxBy(_.toPairs(sumLikes), pair => pair[1])
+
+  return { author: maxLikesAuthor[0], likes: maxLikesAuthor[1] }
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
